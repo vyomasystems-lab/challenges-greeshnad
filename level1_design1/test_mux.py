82ddcd91c4eca4651ca89a9a36fa341d3fd7ad2b
greeshnad <greeshmanthnadella@gmail.com>
2022-07-20 14:22:44 +0000

    test_check

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,5 +7,15 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    for i in range(0,31):
+        inp[i]=random.randint(0, 3)
+    dut.inp0.value=inp[0]
+    selection=0
+    dut.sel.value=selection
+    await Timer(2, units='ns')
+
+    assert dut.out.value == inp[selection], "Randomised test failed with: {A} + {B} = {SUM}".format(
+            A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
+
 
     cocotb.log.info('##### CTB: Develop your test here ########')
