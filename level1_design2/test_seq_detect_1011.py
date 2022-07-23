048727fd6f022e7389f4b5bb74f526bcc4329cf6
greeshnad <greeshmanthnadella@gmail.com>
2022-07-23 15:28:47 +0000

    change

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -26,7 +26,7 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
     dut._log.info('current_state_after_rst=%s',dut.current_state.value)
 
-    
+    #test_vector
     await Timer(10, units='us')
     dut.inp_bit.value=1
     dut._log.info('current_state_bit1=%s',dut.current_state.value)
@@ -41,10 +41,21 @@ async def test_seq_bug1(dut):
     dut._log.info('current_state_bit1=%s',dut.current_state.value)
 
     await Timer(10, units='us')
+    dut.inp_bit.value=0
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
+
+    await Timer(10, units='us')
     dut.inp_bit.value=1
     dut._log.info('current_state_bit1=%s',dut.current_state.value)
 
     await Timer(10, units='us')
+    dut.inp_bit.value=1
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
+
+
+
+    await Timer(10, units='us')
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
     assert dut.seq_seen.value == 1, "Multiplexer test failed with: {select} = {output}".format(
             select=dut.inp_bit.value,  output=dut.seq_seen.value)
     
