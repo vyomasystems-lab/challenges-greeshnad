bd611ee4086c07176cb680f0de686b923f1d6d5e
greeshnad <greeshmanthnadella@gmail.com>
2022-07-23 12:14:28 +0000

    change

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -27,4 +27,29 @@ async def test_mux(dut):
     assert dut.out.value == (input_1), "Multiplexer test failed with: {select} = {output}".format(
             select=dut.sel.value,  output=dut.out.value)
     
+    input_0=3
+    dut.inp10.value=input_0
+    selection=10
+    dut.sel.value=selection
+    await Timer(2, units='ns')
+    dut._log.info(f'Mux_sel={selection} Test_input={input_0} Output_Mux={int(dut.out.value)}')
+    assert dut.out.value == (input_0), "Multiplexer test failed with: {select} = {output}".format(
+            select=dut.sel.value,  output=dut.out.value)
     
+    input_0=3
+    dut.inp10.value=input_0
+    selection=10
+    dut.sel.value=selection
+    await Timer(2, units='ns')
+    dut._log.info(f'Mux_sel={selection} Test_input={input_0} Output_Mux={int(dut.out.value)}')
+    assert dut.out.value == (input_0), "Multiplexer test failed with: {select} = {output}".format(
+            select=dut.sel.value,  output=dut.out.value)   
+
+    input_0=3
+    dut.inp10.value=input_0
+    selection=10
+    dut.sel.value=selection
+    await Timer(2, units='ns')
+    dut._log.info(f'Mux_sel={selection} Test_input={input_0} Output_Mux={int(dut.out.value)}')
+    assert dut.out.value == (input_0), "Multiplexer test failed with: {select} = {output}".format(
+            select=dut.sel.value,  output=dut.out.value)
