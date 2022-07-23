9f6a2d11f2f2c5d5914082329675856856c7811b
greeshnad <greeshmanthnadella@gmail.com>
2022-07-23 13:51:37 +0000

    seq_detector_test

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -7,6 +7,7 @@ import random
 from pathlib import Path
 
 import cocotb
+from cocotb.triggers import Timer
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 
@@ -19,19 +20,31 @@ async def test_seq_bug1(dut):
 
     # reset
     dut.reset.value = 1
-    await FallingEdge(dut.clk)  
+    await FallingEdge(dut.clk)
+    dut._log.info('current_state_before_rst=%s',dut.current_state.value)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
+    dut._log.info('current_state_after_rst=%s',dut.current_state.value)
 
-    input=1
-    dut.inp_bit.value=input
-    input=0
-    dut.inp_bit.value=input
-    input=1
-    dut.inp_bit.value=input
-    input=1
-    dut.inp_bit.value=input
+    
+    await Timer(10, units='us')
+    dut.inp_bit.value=1
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
 
+
+    await Timer(10, units='us')
+    dut.inp_bit.value=0
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
+    
+    await Timer(10, units='us')
+    dut.inp_bit.value=1
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
+
+    await Timer(10, units='us')
+    dut.inp_bit.value=1
+    dut._log.info('current_state_bit1=%s',dut.current_state.value)
+
+    await Timer(10, units='us')
     assert dut.seq_seen.value == 1, "Multiplexer test failed with: {select} = {output}".format(
             select=dut.inp_bit.value,  output=dut.seq_seen.value)
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+    
