84e538be5c136f05cccb435d8ec17b9defc7d9ee
greeshnad <greeshmanthnadella@gmail.com>
2022-08-01 13:56:42 +0000

    error_screenshots

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -58,65 +58,40 @@ def run_test(dut):
         instruction=opcode_1
         for func3_1 in range(0,8):
             instruction1=(opcode_1>>12)| func3_1
-            # print("func3_loop",instruction1)
             instruction=(instruction1<<12)|opcode_1
-            # print(instruction)
+            
             for imm in range(0,32):
                 instruction1=(instruction>>20)| imm
                 instruction2=(instruction1<<20)| instruction
                 for func7_1 in range(0,128):
                     instruction3=(instruction2>>25)| func7_1
-                    # print("func7_loop",instruction1)
+                    
                     instruction_final=instruction3<<25|instruction2
-                    # print("Inside loop",type(instruction_final))
-                    # instruction_hex=hex(instruction_final)
-                    # print("is it printing",hex(instruction_final))
-                    
-                    
                     expected_mav_putvalue1 = bitmanip(instruction_final, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
-                    # print("Inside loop model value",expected_mav_putvalue1)
+                    
                     if expected_mav_putvalue1=='INVALID':
-                        
-                        # i=i+1
-                        # print("i value",i)
                         continue 
-                    # if(func3_1==7 and func7_1==127):
-                    #         break
+                    
                     dut.mav_putvalue_instr.value = instruction_final
                     
-                    # print(instruction_hex)
-                    # await Timer(2,"ns")
+                    
                     yield Timer(1)
-                    # i=i+1
-                    # print(i)
-                    # print(instruction_hex) 
+                     
                     dut_output1 = dut.mav_putvalue.value
-                    # print("Buggy",dut_output1)
-                    # cocotb.log.info(f'DUT OUTPUT={hex(dut_output1)}')
-                    # cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue1)}')
-
+            
                     # comparison
-                    error_message = f'Value mismatch DUT = {hex(dut_output1)} does not match MODEL = {hex(expected_mav_putvalue1)} at opcode={hex(instruction_final)}'
+                    error_message = f'Value mismatch DUT = {hex(dut_output1)} does not match MODEL = {hex(expected_mav_putvalue1)} at instruction={hex(instruction_final)}'
                     try:
                         assert dut_output1 == expected_mav_putvalue1, error_message
-                    except AssertionError as e:
+                    except AssertionError:
                         i=i+1
-                        errors.append(hex(instruction_final))
-                        # cocotb.log.info(f'Value mismatch DUT = {hex(dut_output1)} does not match MODEL = {hex(expected_mav_putvalue1)} at opcode={hex(instruction_final)}')
+                        errors.append(hex(instruction_final))#stores all exception errors i.e model value is not equal to DUT value
+                       
 
-    # dut.mav_putvalue_instr.value = mav_putvalue_instr
-    # yield Timer(1) 
-
-    # # obtaining the output
-    # dut_output = dut.mav_putvalue.value
-    # # valid_bit= dut.RDY_mav_putvalue.value
-    # # print("Valid bit:",valid_bit)
-
-    # cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
-    # cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
     print('i value',i)
-    # print(errors)
+    # convert all errors into hex int value
     errors_int = [int(numeric_string,16) for numeric_string in errors]
+    #run all the error instructions and check which operation it corresponds to
     for instruc in errors_int:
         expected_mav_putvalue1 = bitmanip(instruc, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
         
@@ -130,7 +105,7 @@ def run_test(dut):
         error_message = f'Value mismatch DUT = {hex(dut_output1)} does not match MODEL = {hex(expected_mav_putvalue1)} at opcode={hex(instruc)}'
         try:
             assert dut_output1 == expected_mav_putvalue1, error_message
-        except AssertionError as e:
+        except AssertionError:
             # i=i+1
             # errors.append(hex(instruction_final))
             cocotb.log.info(f'Value mismatch DUT = {hex(dut_output1)} does not match MODEL = {hex(expected_mav_putvalue1)} at opcode={hex(instruc)}')
